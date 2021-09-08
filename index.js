56aa62b83fa003ba0356aec19b3a1e87f86f149a
Shantel <snchristian8@gmail.com>
2021-09-08 10:02:34 -0400

    commit tree 05ee57d25d48b6ea0c380e42eddc24eed362ef87

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.substring(0,1).toUpperCase() + string.substring(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-" "']+/g, '',)
+  }
+
+static titleize(string) {
+  const forbidden = ['a', 'an', 'the','but', 'of', 'and', 'for', 'at', 'by', 'from'];
+  const stringArr = string.split(" ");
+  const cappedStringArr = stringArr.map(function(word, index){
+      if(!forbidden.includes(word) || index === 0) {
+        return word.substring(0,1).toUpperCase() + word.substring(1)
+    } else {
+        return word;
+    }
+
+  });
+ 
+     const resultString =  cappedStringArr.join(" ");
+
+  return resultString;
+
+
+};
 }
